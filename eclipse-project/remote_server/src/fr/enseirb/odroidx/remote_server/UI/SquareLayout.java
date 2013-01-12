696510ee3773b4d302fc1ce4d2fb9991a696ef1c
sylvain <sylvain@mutex>
2013-01-13 00:19:35 +0100

    forget to add images :)

---
+++ eclipse-project/remote_server/src/fr/enseirb/odroidx/remote_server/UI/SquareLayout.java
@@ -0,0 +1,17 @@
+package fr.enseirb.odroidx.remote_server.UI;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class SquareLayout extends LinearLayout {
+
+    public SquareLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+    protected void onMeasure (int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
